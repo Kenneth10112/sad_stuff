3818f1b6de444b8b0f143c0560b085bd4d5dded9
vei. <rbey.sicat@gmail.com>
2025-04-14 18:36:29 +0800

    Integrated DB objects in Accoutant View

--- digiledger/views.py
+++ digiledger/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .models import FinancialTransactions
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return render(request, 'digiledger/index.html')
 
+@csrf_exempt
 def accountant(request):
     return render(request, 'digiledger/accountant.html')
 
 def supervisor(request):
     return render(request, 'digiledger/supervisor.html')
 
+@csrf_exempt
 def login(request):
     if request.method == "POST":
         uname = request.POST['username']
         upass = request.POST['password']
 
         if uname == "kenneth" and upass == "amboya":
-            return redirect("digiledger:accountant")
+            transactions = FinancialTransactions.objects.all()
+            return render(request, "digiledger/accountant.html", {"transactions": transactions})
         else:
             return render(request, "digiledger/login.html", {"is_invalidLogin": True})
 
