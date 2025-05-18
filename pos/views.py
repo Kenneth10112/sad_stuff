e8ea1ef3a52c47466c3d335cbb58e59d73393b5a
Kenneth <kennethamboya101@gmail.com>
2025-05-19 06:31:19 +0800

    final update
    gg na bahala na batmap

--- pos/views.py
+++ pos/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return httpresponse("this is the index")
+
+    return render(request, 'pos/pos.html')
